b8b1424a79d6ea19b098983f7287bbdb563614fb
bulatko <buloch03@yandex.ru>
2019-10-19 12:32:54 +0300

    gamepark complete

--- gameparkParser.php
+++ gameparkParser.php
@@ -3,7 +3,7 @@
 if(!isset($_GET['url']))exit();
 
 require_once 'functions.php';
-
+require_once 'phpQuery.php';
 $url = $_GET['url'];
 
 $data = get_content($url);
@@ -13,4 +13,6 @@ $selector = '#eprice';
 $doc = phpQuery::newDocument($data);
 
 $answer = $doc->find($selector)->text();
+
+echo $answer;
 
