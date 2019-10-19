8bc2ae54d42e221b8cea9e3c86f7690499dc679d
bulatko <buloch03@yandex.ru>
2019-10-19 12:36:39 +0300

    igromagaz complete

--- igromagaz.php
+++ igromagaz.php
@@ -1,2 +1,17 @@
 <?php
 
+if(!isset($_GET['url']))exit();
+
+require_once 'functions.php';
+require_once 'phpQuery.php';
+$url = $_GET['url'];
+
+$data = get_content($url);
+
+$selector = 'body > div.page-wrap > div.catalog-detail > div.detail-right.container-detail-js_36331 > ' .
+    'div.price.detail-right__price > div > div.price__val';
+$doc = phpQuery::newDocument($data);
+
+$answer = $doc->find($selector)->text();
+
+echo $answer;
