6591f77d0dd4b32258cda3c20dfac258be6b7513
bulatko <buloch03@yandex.ru>
2019-10-19 17:32:20 +0300

    roxen complete, steambuy update

--- roxen.php
+++ roxen.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if(!isset($_GET['url']))exit();
 
 require_once 'functions.php';
@@ -9,16 +8,16 @@ $url = $_GET['url'];
 
 $data = get_content($url);
 
-$selector1 = 'body > div.wrapper > div.container > div.product-wrap > div.product-main > div.product-main__foot > div > div.product-price__action > a';
+$selector1 = 'body > div.r-wrap > div.r-content-wrap > div > div > div:nth-child(2) > section > article > div.r-buy-wrap > div:nth-child(2)';
 
 
 $selector = 'body > div.r-wrap > div.r-content-wrap > div > div > div:nth-child(2) > section > article > div.r-buy-wrap > div.r-prices > div';
 
 $doc = phpQuery::newDocument($data);
-if($doc->find($selector1)->text()=='Нет в наличии'){
+if($doc->find($selector1)->text()=='нет в наличии'){
     $answer = '';
 }
 else
-    $answer = $doc->find($selector)->text();
+    $answer = $doc->find($selector)->text() . " Р";
 
 echo $answer;
